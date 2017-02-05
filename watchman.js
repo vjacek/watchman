1911e8f6e9cccdc0d5fec5a88c74cf6357007c06
Victor Jacek <victorjacek@gmail.com>
2017-02-05 01:44:38 +0000

    RPI sync

--- watchman.js
+++ watchman.js
@@ -1,12 +1,18 @@
 var cv = require('opencv');
 var lines = new cv.Matrix();
-var camera = new cv.VideoCapture(0);
+//var camera = new cv.VideoCapture(0);
+var RaspiCam = require('raspicam');
 var io = require('socket.io-client');
 var os = require('os');
 
+var camera = new RaspiCam({
+    mode: 'video',
+    output: '-'
+});
+
 // Connect to the Castle server
-//var socket = io.connect('http://192.168.1.11:8080');
-var socket = io.connect('http://127.0.0.1:8080');
+var socket = io.connect('http://192.168.1.103:8080');
+//var socket = io.connect('http://127.0.0.1:8080');
 
 // Each Watchman client has a unique identifier
 var hostname = os.hostname();
@@ -17,9 +23,17 @@ hostname = hostname.replace('.local');
 // < 10ms timeout appears to be too fast for sending via sockets
 // 1000 / 35 = 28.57 frames/sec
 // 1000 / 100 = 10 frames/sec
-var fps = 10;
+var fps = 30;
 var rate = 1000 / fps;
-setInterval(capture(), 100);
+
+camera.start();
+
+camera.on("read", function(err, timestamp, filename){
+    //do stuff
+});
+
+
+//setInterval(capture, 1);
 
 
 
@@ -27,7 +41,17 @@ function capture() {
 
     // Get the next frame and send it to the server
     camera.read(function(error, matrix) {
-        socket.emit('watchman', {id: hostname, imageBuffer: matrix.toBuffer()});
+
+	console.log(matrix.size());
+	
+	// TODO Change to try/catch
+	if(!error) {	
+            socket.emit('watchman', {id: hostname, imageBuffer: matrix.toBuffer()});
+	}
+	else {
+	    console.log(error);
+	}
+	    
     });
 
 }
