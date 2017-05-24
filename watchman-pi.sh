54be5268046002a454f8d79d72462015ba452be9
Victor Jacek <victorjacek@gmail.com>
2017-05-24 03:15:33 +0000

    Locate using nodejs

--- watchman-pi.sh
+++ watchman-pi.sh
@@ -1,32 +1,7 @@
 # Automatically locate the Castle server
 # Port is always fixed at 8888, but DHCP network may change IPs
 
-#!/bin/bash
-
-subnet="192.168.0."
-ip=200
-max=210
-port=":8888"
-path="/locate"
-
-echo "Watchman startup... "
-echo "Looking for Castle..."
-
-while (("$ip" < "$max"))
-do
-    address="$subnet$ip$port$path"
-    echo "Looking for Castle at $subnet$ip"
-    result=$(curl -s --connect-timeout 1 $address)
-    if [ "$result" == "true" ]
-    then
-        castleIP="$subnet$ip"
-        echo "Found Castle at $subnet$ip"
-        break
-    fi
-    ((ip++))
-done
-
-# Start the video camera and send each image to the Castle server
+# Start the video camera and send each image to a local UNIX domain socket
 
 # -o -      : Write to stdout
 # -t 0      : Capture indefinitely
@@ -36,6 +11,5 @@ done
 # -g 1      : Make every frame a key frame (TODO: test if this is still needed, could reduce frame size if not needed)
 # -fl       : Flush buffer after every capture, reduces latency and makes image splitting cleaner
 # -cd MJPEG : Motion JPEG formats capture frames as JPEG images
-# -vf       : Flip image vertically (camera is mounted upside down)
-#raspivid -o - -t 0 -w 1024 -h 768 -fps 10 -g 1 -fl -cd MJPEG -vf | node send.js $castleIP
-raspivid -o - -t 0 -w 1024 -h 768 -fps 10 -g 1 -fl -cd MJPEG -vf | nc -U "/tmp/watchman.sock"
+
+raspivid -o - -t 0 -w 1024 -h 768 -fps 10 -g 1 -fl -cd MJPEG | nc -U "/tmp/watchman.sock"
